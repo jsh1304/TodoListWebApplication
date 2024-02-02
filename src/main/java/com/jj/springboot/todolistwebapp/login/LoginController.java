2e5ba03b3f07b2e3c9353e59ca534c9ae0019d2a
jsh1304 <jung3914a@gmail.com>
2024-02-02 11:37:11 +0900

    Docs: LoginController에 대한 작업 흐름 설명 추가

--- src/main/java/com/jj/springboot/todolistwebapp/login/LoginController.java
+++ src/main/java/com/jj/springboot/todolistwebapp/login/LoginController.java
@@ -7,19 +7,21 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
+/**
+ * 1. 알맞은 Controller 메서드 식별 : login -> LoginController.gotoLoginPage
+ * 2. Controller 메서드 실행 -> data를 model에 put한다. view name 리턴한다.
+ * 3. 알맞은 뷰를 식별 -> 디스패처 서블릿은 뷰 리졸버를 사용
+ * 4. 뷰를 실행
+ * **/
 @Controller
 public class LoginController {
 
-    private Logger logger = LoggerFactory.getLogger(getClass());
+
 
     @RequestMapping("login")
     public String gotoLoginPage(@RequestParam String name, ModelMap model) {
         model.put("name", name);
-        logger.debug("Request param is {}", name);
-        logger.info("Request param is {}", name);
-        logger.warn("Request param is {}", name);
-        System.out.println("Request param is" + name);
+
         return "login";
     }
 }
