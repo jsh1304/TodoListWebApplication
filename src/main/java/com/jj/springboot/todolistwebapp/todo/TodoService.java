e4994135cab72eae02a08c3dead19951ec581689
jsh1304 <jung3914a@gmail.com>
2024-02-16 11:49:32 +0900

    docs: TodoService에 deleteById, findById 주석 추가

--- src/main/java/com/jj/springboot/todolistwebapp/todo/TodoService.java
+++ src/main/java/com/jj/springboot/todolistwebapp/todo/TodoService.java
@@ -32,14 +32,16 @@ public class TodoService {
     }
 
     public void deleteById(int id) {
-
-        Predicate<? super Todo> predicate
-                = todo -> todo.getId() == id;
+        // 투두 객체의 id가 사용자가 찾는 id와 같은지 확인
+        Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+        // todos 컬렉션에서 해당 투두 객체를 제거
         todos.removeIf(predicate);
     }
 
     public Todo findById(int id) {
+        // 투두 객체의 id가 사용자가 찾는 id와 같은지 확인
         Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+        // 스트림 API를 사용하여 todos 컬렉션을 필터링. 해당하는 첫 번째 투두 객체 찾기
         Todo todo = todos.stream().filter(predicate).findFirst().get();
         return todo;
     }
