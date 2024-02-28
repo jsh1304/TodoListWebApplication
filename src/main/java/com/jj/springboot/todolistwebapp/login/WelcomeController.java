13834bc3198b99e3afec3f9feeb140b8ddad6ed8
jsh1304 <jung3914a@gmail.com>
2024-02-28 11:04:09 +0900

    feat: 로그인 JSP 파일 삭제 및 WelcomeController 수정
    -로그인 JSP 파일 삭제
    -Login.jsp -> WelcomeController 이름 수정

--- src/main/java/com/jj/springboot/todolistwebapp/login/WelcomeController.java
+++ src/main/java/com/jj/springboot/todolistwebapp/login/WelcomeController.java
@@ -1,7 +1,5 @@
 package com.jj.springboot.todolistwebapp.login;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,18 +15,12 @@ import org.springframework.web.bind.annotation.SessionAttributes;
  * **/
 @Controller
 @SessionAttributes("name")
-public class LoginController {
+public class WelcomeController {
 
-    private AuthenticationService authenticationService;
-
-    public LoginController(AuthenticationService authenticationService) {
-        super();
-        this.authenticationService = authenticationService;
-    }
-
-    @RequestMapping(value = "login", method = RequestMethod.GET)
-    public String gotoLoginPage() {
-        return "login";
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String gotoLoginPage(ModelMap model) {
+        model.put("name", "jj");
+        return "welcome";
     }
 
     /**
@@ -36,20 +28,5 @@ public class LoginController {
      * 사용자 자격 증명을 확인하고 인증이 성공하면 환영 페이지로 리디렉션
      * 그렇지 않으면 로그인 페이지에 오류 메시지를 표시
      */
-    @RequestMapping(value = "login", method = RequestMethod.POST)
-    public String gotoWelcomePage(@RequestParam String name,
-                                  @RequestParam String password,
-                                  ModelMap model) {
-
-        if (authenticationService.authenticate(name, password)) {
-            model.put("name", name);
-            model.put("password", password);
-            return "welcome";
-        }
-
-        model.put("errorMessage", "잘못된 인증정보");
-        return "login";
-    }
-
 
 }
