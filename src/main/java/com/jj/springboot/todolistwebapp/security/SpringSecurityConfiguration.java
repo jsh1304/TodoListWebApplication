902a8940e060b186e307ba8707d378bc95d1fe8b
jsh1304 <jung3914a@gmail.com>
2024-03-04 12:36:53 +0900

    refactor: Spring Security 구성 클래스에서 사용자 생성 메서드 추출
    -Spring Security 구성 클래스(SpringSecurityConfiguration)의 createUserDetailsManager 메서드에서 사용자 생성 코드를 별도의 메서드 createNewUser로 추출

--- src/main/java/com/jj/springboot/todolistwebapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/jj/springboot/todolistwebapp/security/SpringSecurityConfiguration.java
@@ -16,19 +16,27 @@ public class SpringSecurityConfiguration {
 
     @Bean
     public InMemoryUserDetailsManager createUserDetailsManager() {
+
+        // 사용자 세부 정보 생성
+        UserDetails userDetails1 = createNewUser("jj", "1234");
+        UserDetails userDetails2 = createNewUser("qq", "1234");
+
+        // InMemoryUserDetailsManager 빈 생성하여 사용자 세부 정보 반환
+        return new InMemoryUserDetailsManager(userDetails1, userDetails2);
+    }
+
+    private UserDetails createNewUser(String username, String password) {
         // 비밀번호 인코딩을 위한 함수 정의
         Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
 
         // 사용자 세부 정보 생성
         UserDetails userDetails = User.builder()
                 .passwordEncoder(passwordEncoder)
-                .username("jj")
-                .password("1234") // 비밀번호 인코딩
+                .username(username)
+                .password(password) // 비밀번호 인코딩
                 .roles("USER", "ADMIN")
                 .build();
-
-        // InMemoryUserDetailsManager 빈 생성하여 사용자 세부 정보 반환
-        return new InMemoryUserDetailsManager(userDetails);
+        return userDetails;
     }
 
     @Bean
